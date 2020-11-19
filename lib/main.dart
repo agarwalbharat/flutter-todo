16f8c9e0cb73b105bf4d772543cf936510b4b515
Bharat Agarwal <agarwalbharat68@gmail.com>
2020-11-19 23:03:19 +0530

    completed basic todo app

--- lib/main.dart
+++ lib/main.dart
@@ -1,117 +1,155 @@
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
 
-void main() {
-  runApp(MyApp());
+void main() async {
+  runApp(MaterialApp(
+    title: "Todos Demo",
+    debugShowCheckedModeBanner: false,
+    home: MyApp(),
+  ));
 }
 
-class MyApp extends StatelessWidget {
-  // This widget is the root of your application.
-  @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // Try running your application with "flutter run". You'll see the
-        // application has a blue toolbar. Then, without quitting the app, try
-        // changing the primarySwatch below to Colors.green and then invoke
-        // "hot reload" (press "r" in the console where you ran "flutter run",
-        // or simply save your changes to "hot reload" in a Flutter IDE).
-        // Notice that the counter didn't reset back to zero; the application
-        // is not restarted.
-        primarySwatch: Colors.blue,
-        // This makes the visual density adapt to the platform that you run
-        // the app on. For desktop platforms, the controls will be smaller and
-        // closer together (more dense) than on mobile platforms.
-        visualDensity: VisualDensity.adaptivePlatformDensity,
-      ),
-      home: MyHomePage(title: 'Flutter Demo Home Page'),
-    );
-  }
-}
-
-class MyHomePage extends StatefulWidget {
-  MyHomePage({Key key, this.title}) : super(key: key);
-
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
-
-  final String title;
+class MyApp extends StatefulWidget {
+  MyApp({Key key}) : super(key: key);
 
   @override
-  _MyHomePageState createState() => _MyHomePageState();
+  _MyAppState createState() => _MyAppState();
 }
 
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
+class _MyAppState extends State<MyApp> {
+  bool _initializedFirebase = false;
 
-  void _incrementCounter() {
+  String _todoItem = "";
+
+  void initializeFirebase() async {
+    await Firebase.initializeApp();
     setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      _counter++;
+      _initializedFirebase = true;
     });
   }
 
   @override
+  void initState() {
+    initializeFirebase();
+    super.initState();
+  }
+
+  void _addTodo() {
+    FirebaseFirestore.instance
+        .collection('todos')
+        .add({"todo": _todoItem})
+        .then((value) => print("Added $value"))
+        .catchError((error) => print(error));
+  }
+
+  void _deleteTodos(String id) {
+    FirebaseFirestore.instance
+        .collection('todos')
+        .doc(id)
+        .delete()
+        .then((value) => print("Delete Success $id"))
+        .catchError((error) => print(error));
+  }
+
+  @override
   Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
-    return Scaffold(
-      appBar: AppBar(
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
-        title: Text(widget.title),
-      ),
-      body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
-        child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Invoke "debug painting" (press "p" in the console, choose the
-          // "Toggle Debug Paint" action from the Flutter Inspector in Android
-          // Studio, or the "Toggle Debug Paint" command in Visual Studio Code)
-          // to see the wireframe for each widget.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            Text(
-              'You have pushed the button this many times:',
+    return (!_initializedFirebase)
+        ? Scaffold(
+            body: Center(
+              child: Text(
+                "Please wait, we are setting up things for you!",
+                style: TextStyle(fontSize: 25),
+              ),
             ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headline4,
+          )
+        : Scaffold(
+            appBar: AppBar(
+              title: Text("Todos"),
             ),
-          ],
-        ),
-      ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
-    );
+            floatingActionButton: FloatingActionButton(
+              onPressed: () {
+                showDialog(
+                    context: context,
+                    builder: (BuildContext context) {
+                      return AlertDialog(
+                        shape: RoundedRectangleBorder(
+                            borderRadius: BorderRadius.circular(8)),
+                        title: Text("Add Todo"),
+                        content: TextField(
+                          decoration: InputDecoration(
+                            border: OutlineInputBorder(),
+                            hintText: "Todo",
+                          ),
+                          onChanged: (String value) {
+                            _todoItem = value;
+                          },
+                        ),
+                        actions: <Widget>[
+                          FlatButton(
+                            onPressed: () {
+                              _addTodo();
+                              Navigator.of(context).pop();
+                            },
+                            child: Text("Add"),
+                          )
+                        ],
+                      );
+                    });
+              },
+              child: Icon(
+                Icons.add,
+                color: Colors.white,
+              ),
+            ),
+            body: StreamBuilder(
+                stream:
+                    FirebaseFirestore.instance.collection("todos").snapshots(),
+                builder: (context, snapshots) {
+                  if (snapshots.hasData) {
+                    return (snapshots.data.documents.length == 0)
+                        ? Center(
+                            child: Text("There is nothing ToDo, Sit back and Relex 😎", style: TextStyle(fontSize: 18),),
+                          )
+                        : ListView.builder(
+                            shrinkWrap: true,
+                            itemCount: snapshots.data.documents.length,
+                            itemBuilder: (context, index) {
+                              DocumentSnapshot documentSnapshot =
+                                  snapshots.data.documents[index];
+                              return Dismissible(
+                                background: Container(
+                                  padding: EdgeInsets.all(8),
+                                  alignment: AlignmentDirectional.centerStart,
+                                  color: Colors.red,
+                                  child: Icon(
+                                    Icons.delete,
+                                    color: Colors.white,
+                                  ),
+                                ),
+                                onDismissed: (direction) {
+                                  _deleteTodos(documentSnapshot.id);
+                                },
+                                key: Key(documentSnapshot.id),
+                                child: Card(
+                                  elevation: 4,
+                                  margin: EdgeInsets.all(8),
+                                  shape: RoundedRectangleBorder(
+                                      borderRadius: BorderRadius.circular(8)),
+                                  child: ListTile(
+                                    title:
+                                        Text(documentSnapshot.data()["todo"]),
+                                  ),
+                                ),
+                              );
+                            });
+                  } else {
+                    return Align(
+                      alignment: FractionalOffset.bottomCenter,
+                      child: CircularProgressIndicator(),
+                    );
+                  }
+                }),
+          );
   }
 }
